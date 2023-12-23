9100af466558c98f120d4559888d94bf4f11faf7
zvvazo <zvvazo@gmail.com>
2023-12-23 19:32:01 +0100

    Data Ingestion

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from mlProject.custom_logger import logger
+from mlProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 
 
+def run_training_pipeline(stage_name, pipeline_class):
+    try:
+        logger.info(f"------  {stage_name} started  ------") 
+        pipeline = pipeline_class()
+        pipeline.main()
+        logger.info(f"------  {stage_name} completed  ------\n\n||==========||")
+    except Exception as e:
+        logger.exception(e)
+        raise e
 
-
-logger.info(f"------  TEST  ------") 
+run_training_pipeline("Data Ingestion stage", DataIngestionTrainingPipeline)
 
 
